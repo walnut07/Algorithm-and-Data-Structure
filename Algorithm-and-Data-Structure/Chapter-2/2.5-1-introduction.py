61ce2078a1a0ef0f69d5e6c71f8d22958510d158
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-05-10 10:19:43 +0900

    Solved this problem

---
+++ Algorithm-and-Data-Structure/Chapter-2/2.5-1-introduction.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+
+minv = int(input())
+maxv = -float("inf")
+
+for i in range(n - 1):
+    r = int(input())
+    maxv = max(maxv, r - minv)
+    minv = min(minv, r)
+
+print(maxv)
+
+# python3 2.5-1-introduction.py
